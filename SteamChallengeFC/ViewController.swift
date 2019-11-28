a24caebfe0e7da25e9a6ae7470ca050436580f39
Michael <polkandra@gmail.com>
2019-11-28 13:46:12 +0300

    Initial Commit

---
+++ SteamChallengeFC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SteamChallengeFC
+//
+//  Created by Michael on 28.11.2019.
+//  Copyright © 2019 Michael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
